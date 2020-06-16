4dd85280e484943c89d8c227eb5d12ad162ddde1
olegmalyarenko <oi.malyarenko@gmail.com>
2020-06-16 19:54:45 +0300

    feat:add basic items

---
+++ start-vue/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    
+   <h1>To do App</h1>
+   <ToDoList
+   v-bind:todos="todos"
+   />
+  </div>
+</template>
+
+<script>
+import ToDoList from '@/components/ToDoList';
+export default {
+  name: 'App',
+  data() {
+   return {
+     todos: [
+       {id: 1,  title: 'Kупить хлеб', comleted: false},
+       {id: 2,  title: 'Kупить масло', comleted: false},
+       {id: 3,  title: 'Kупить сало', comleted: false}
+
+     ]
+   }
+  },
+  components: {
+    ToDoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ start-vue/src/components/ToDoList.vue
@@ -0,0 +1,23 @@
+<template>
+ <div>
+ <ul> 
+   <ToDoItem 
+     v-for= "todo of todos"
+     v-bind:todo="todo"
+   />
+ 
+ </ul>
+ </div>
+</template>
+
+<script> 
+import ToDoItem from '@/components/ToDoItem'
+export default {
+    props: ['todos'],
+    components: {
+    ToDoItem
+  }
+}
+</script>
+
+<style></style>
